ab6b4e4ba414696ce48b0e280f1a940de130ff60
adilmayank <adilmayank5894@gmail.com>
2023-06-18 15:56:13 +0530

    adjusting control center width

--- src/components/ControlCenter.js
+++ src/components/ControlCenter.js
@@ -7,13 +7,13 @@ import SortButton from './Controls/SortButton'
 const ControlCenter = () => {
   return (
     <div className="control-center-container px-2 py-2 frosted justify-center rounded-lg flex w-5/6">
-      <div className="control-center py-4 grid grid-cols-4 2xl:w-5/6">
+      <div className="control-center p-4 grid grid-cols-4 w-full">
         <AlgorithmSelector />
         <ArraySizeSelector />
         <SortingSpeedSelector />
         <div
           className="action-buttons-container justify-evenly items-end
-               grid grid-cols-2 gap-2 mx-2 w-full"
+               grid grid-cols-2 gap-2 w-full"
         >
           <RandomizeButton />
           <SortButton />
