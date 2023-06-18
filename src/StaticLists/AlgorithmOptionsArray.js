533be160fc9280aada81c6936ef96095e1ae245c
adilmayank <adilmayank5894@gmail.com>
2023-06-18 12:49:55 +0530

    removed unnecessary variable

--- src/StaticLists/AlgorithmOptionsArray.js
+++ src/StaticLists/AlgorithmOptionsArray.js
@@ -1,11 +1,5 @@
-export const AlgorithmOptionsArray = [
-  { name: 'insertionSort', text: 'Insertion Sort', active: true },
-  { name: 'mergeSort', text: 'Merge Sort', active: false },
-  { name: 'quickSort', text: 'Quick Sort', active: false },
-]
-
 export const AlgorithmOptions = {
-  insertionSort: { text: 'Insertion Sort', active: true },
+  insertionSort: { text: 'Insertion Sort', active: false },
   mergeSort: { text: 'Merge Sort', active: false },
-  heapSort: { text: 'Heap Sort', active: false },
+  heapSort: { text: 'Heap Sort', active: true },
 }
