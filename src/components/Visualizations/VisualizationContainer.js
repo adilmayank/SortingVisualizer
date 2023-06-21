c9b27f513c3a92ca64668eeccca4c73f0ad288a5
adilmayank <adilmayank5894@gmail.com>
2023-06-21 21:57:12 +0530

    Moved parent container's div to Visualizer Container Component

--- src/components/Visualizations/VisualizationContainer.js
+++ src/components/Visualizations/VisualizationContainer.js
@@ -3,10 +3,10 @@ import BarsArea from './BarsArea'
 
 const VisualizationContainer = () => {
   return (
-    <>
+    <div className="visuzalization-container grid grid-cols-12 my-14 w-5/6 h-full">
       <BarsArea />
       <AdditionalVisualizationArea />
-    </>
+    </div>
   )
 }
 export default VisualizationContainer
