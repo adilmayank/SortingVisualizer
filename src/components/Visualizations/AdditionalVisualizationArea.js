eb5678cd50d2c699437043491d1952ba5c0071f6
adilmayank <adilmayank5894@gmail.com>
2023-06-21 21:57:50 +0530

    Centered Text and Responsive size

--- src/components/Visualizations/AdditionalVisualizationArea.js
+++ src/components/Visualizations/AdditionalVisualizationArea.js
@@ -2,7 +2,7 @@ const AdditionalVisualizationArea = () => {
   return (
     <div className="additional-visualization-container col-span-4 ml-3 frosted h-full rounded-lg">
       <div className="additional-visualization flex flex-col justify-center items-center h-full w-full">
-        <p className="text-2xl font-bold uppercase my-1">
+        <p className="font-bold uppercase my-1 flex justify-center p-1 text-center lg:text-lg sm:text-md xl:text-xl ">
           Additional Visualizations
         </p>
         <p className="text-2xl uppercase my-1">👀 Coming Soon 👀</p>
