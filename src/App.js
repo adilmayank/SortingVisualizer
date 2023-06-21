6ade5013ea80fe6524338a0052285571ec9ea165
adilmayank <adilmayank5894@gmail.com>
2023-06-21 21:32:04 +0530

    Updated Component during Componentizing App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import SortingBox from './components/SortingBox'
+import AppTitle from './components/AppTitle'
+import ControlCenter from './components/Controls/ControlCenter'
+import VisualizationContainer from './components/Visualizations/VisualizationContainer'
 import { ContextProvider } from './Context/AppContext'
 
 function App() {
@@ -6,12 +8,13 @@ function App() {
     <ContextProvider>
       <div className="app-bg min-h-[700px] min-w-[1150px] h-screen backdrop-blur-md font-sans">
         <div className="main-container py-2 w-full h-full flex flex-col">
-          <div className="title-container heading m-3 h-20 flex justify-center items-center">
-            <p className="text-2xl font-bold uppercase text-black p-3 border-2 border-black">
-              Sorting Visuazlier
-            </p>
+          <AppTitle />
+          <div className="content-container my-3 p-3 flex flex-col h-full w-full items-center">
+            <ControlCenter />
+            <div className="visuzalization-container grid grid-cols-12 my-14 w-5/6 h-full">
+              <VisualizationContainer />
+            </div>
           </div>
-          <SortingBox />
         </div>
       </div>
     </ContextProvider>
