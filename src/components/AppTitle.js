ac4cea62c40db9e1e36b257a87e5aabb56a1efc1
adilmayank <adilmayank5894@gmail.com>
2023-06-23 14:29:15 +0530

    Fixed "Visualizer" Spelling Typo

--- src/components/AppTitle.js
+++ src/components/AppTitle.js
@@ -2,7 +2,7 @@ const AppTitle = () => {
   return (
     <div className="title-container heading m-3 h-20 flex justify-center items-center">
       <p className="text-2xl font-bold uppercase text-black p-3 border-2 border-black">
-        A Sorting Visuazlier
+        A Sorting Visualizer
       </p>
     </div>
   )
